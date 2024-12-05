c095980b5a8268f92f99a0ac1f6ac9b7ee5f81c1
Rafael Oliveira Rosário <rafaelor20@hotmail.com>
2024-12-05 10:34:53 -0300

    .

--- script_docker.sh
+++ script_docker.sh
@@ -1,2 +1,5 @@
+# git clone https://github.com/rafaelor20/Lab-Redes.git
+#
+# script para criar a imagem e rodar o container do site
 docker build -t zootecnia_site .
 docker run -d -p 63000:80 zootecnia_site
